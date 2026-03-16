e489480a23c47b24f38fda16893fbb9fc573e585
Daniel Nylander <daniel@danielnylander.se>
2026-03-16 18:20:32 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="storymaker",
-    version="1.0.0",
+    version="0.2.0",
     description="Interactive AI-powered stories with pictograms for reading comprehension training",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
