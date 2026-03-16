3115d016cb09c814b76e6a070d1ccb4abb767573
Daniel Nylander <daniel@danielnylander.se>
2026-03-16 16:11:30 +0100

    i18n: proper locale/gettext setup with LANG support

---
+++ storymaker/i18n.py
@@ -0,0 +1,30 @@
+import gettext
+import locale
+import os
+
+DOMAIN = "storymaker"
+LOCALEDIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "locale")
+
+# System locale dirs
+SYSTEM_LOCALEDIRS = [
+    LOCALEDIR,
+    "/usr/share/locale",
+    "/usr/local/share/locale",
+]
+
+try:
+    locale.setlocale(locale.LC_ALL, "")
+except locale.Error:
+    pass
+
+# Find locale dir with our translations
+for d in SYSTEM_LOCALEDIRS:
+    if os.path.isdir(d):
+        gettext.bindtextdomain(DOMAIN, d)
+        break
+else:
+    gettext.bindtextdomain(DOMAIN, LOCALEDIR)
+
+gettext.textdomain(DOMAIN)
+_ = gettext.gettext
+
